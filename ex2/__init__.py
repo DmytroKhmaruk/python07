9496193f0a4128f30dc39ad94cd20da95278accc
dkhmaruk <dkhmaruk@student.42vienna.com>
2026-08-08 12:07:52 +0200

    finish ex2

--- ex2/__init__.py
+++ ex2/__init__.py
@@ -1,5 +1,6 @@
 from .strategy import (
-    NormalStrategy, AggressiveStrategy, DefensiveStrategy, BattleStrategy)
+    NormalStrategy, AggressiveStrategy, DefensiveStrategy, BattleStrategy,
+    InvalidStrategyError)
 
-__all__ = [NormalStrategy, AggressiveStrategy, DefensiveStrategy,
-           BattleStrategy]
+__all__ = ["NormalStrategy", "AggressiveStrategy", "DefensiveStrategy",
+           "BattleStrategy", "InvalidStrategyError"]
--- tournament.py
+++ tournament.py
@@ -1,8 +1,13 @@
-from ex0 import CreatureFactory
-from ex2 import NormalStrategy, AggressiveStrategy, DefensiveStrategy, BattleStrategy
+from ex0 import CreatureFactory, FlameFactory, AquaFactory
+from ex1 import HealingCreatureFactory, TransformCreatureFactory
+from ex2 import (
+    NormalStrategy, AggressiveStrategy, DefensiveStrategy, BattleStrategy,
+    InvalidStrategyError)
 
 
 def battle(opponents: list[tuple[CreatureFactory, BattleStrategy]]) -> None:
+    print("*** Tournament ***")
+    print(f"{len(opponents)} opponents involved")
     for i in range(len(opponents)):
         for j in range(i + 1, len(opponents)):
             first_factory, first_strategy = opponents[i]
@@ -10,8 +15,48 @@ def battle(opponents: list[tuple[CreatureFactory, BattleStrategy]]) -> None:
             first_creature = first_factory.create_base()
             second_creature = second_factory.create_base()
 
-            print("* Battle *")
+            print("\n* Battle *")
             print(first_creature.describe())
             print(" vs.")
             print(second_creature.describe())
             print(" now fight!")
+
+            try:
+                first_strategy.act(first_creature)
+                second_starategy.act(second_creature)
+            except InvalidStrategyError as e:
+                print(e)
+                return
+
+
+if __name__ == "__main__":
+    flame_factory = FlameFactory()
+    aqua_factory = AquaFactory()
+    healing_factory = HealingCreatureFactory()
+    transform_factory = TransformCreatureFactory()
+
+    normal = NormalStrategy()
+    aggressive = AggressiveStrategy()
+    defensive = DefensiveStrategy()
+
+    print("Tournament 0 (basic)")
+    print(" [ (Flameling+Normal), (Healing+Defensive) ]")
+    tournament_0 = [
+        (flame_factory, normal),
+        (healing_factory, defensive)]
+    battle(tournament_0)
+
+    print("\nTournament 1 (error)")
+    print(" [ (Flameling+Aggressive), (Healing+Defensive) ]")
+    tournament_1 = [
+        (flame_factory, aggressive),
+        (healing_factory, defensive)]
+    battle(tournament_1)
+
+    print("\nTournament 2 (multiple)")
+    print(" [ (Aquabub+Normal), (Healing+Defensive), (Transform+Aggressive) ]")
+    tournament_2 = [
+        (aqua_factory, normal),
+        (healing_factory, defensive),
+        (transform_factory, aggressive)]
+    battle(tournament_2)
